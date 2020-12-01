5508da145686a88641a281fec9cfef5505ebd3d9
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-12-01 17:04:45 +0200

    Feature: Adding basic layout
    Centered vertically and horizontally for ease of implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+import { StoreProvider } from './store';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vw;
+  width: 100vw;
+`;
+
+function App(): React.ReactElement {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <StoreProvider>todos!</StoreProvider>
+    </Container>
   );
 }
 
