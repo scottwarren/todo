78e291f065522aadafa27512bef882e83426625a
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-12-02 21:24:50 +0200

    Feature: Adding module for our database integration

---
+++ src/store/database.ts
@@ -0,0 +1,17 @@
+import Dexie from 'dexie';
+
+import { IToDo } from './';
+
+export class ToDoDatabase extends Dexie {
+  todos: Dexie.Table<IToDo, number>;
+
+  constructor(databaseName: string) {
+    super(databaseName);
+    this.version(1).stores({
+      todos: '++id,title,isCompleted',
+    });
+
+    // Just informing Typescript what Dexie has already done
+    this.todos = this.table('todos');
+  }
+}
