2ccdbaeef7d226e724f4b0a9a0557dffe4598ba5
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2020-12-03 17:41:42 +0200

    Chore: Linting

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-import { ToDoDatabase, db } from './database';
+import { db } from './database';
 
 export interface IToDo {
   id: string;
